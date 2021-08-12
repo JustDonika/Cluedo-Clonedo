54d2116518eb934bd8c183ea9bd371686c4d73b5
JustDonika <86822565+JustDonika@users.noreply.github.com>
2021-08-12 16:44:48 +1200

    Delete Assignment2 directory

--- Assignment2/Features/MurderMystery.java
+++
@@ -1,9 +0,0 @@
-/**
- * Activator class. Separation from GameModel allows for non-static behaviour.
- */
-public class MurderMystery {
-    public static void main(String args[]){
-        GameModel GM = new GameModel();
-        GM.main(null);
-    }
-}
