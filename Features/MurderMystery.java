f77e7d94e8d84021ab7b212e9fbfc704681d5355
tulldono <tulldono@ecs.vuw.ac.nz>
2021-08-13 19:10:01 +1200

    That was scuffed

---
+++ Features/MurderMystery.java
@@ -0,0 +1,11 @@
+import java.awt.event.*;
+
+/**
+ * Activator class. Separation from GameModel allows for non-static behaviour.
+ */
+public class MurderMystery{
+    public static void main(String args[]){
+        GameModel GM = new GameModel();
+        GM.main(null);
+    }
+}
